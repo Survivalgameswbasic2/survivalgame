39f9911ddebb95c2601c4baf0d0b8df867765937
asdf6864 <134824544+asdf6864@users.noreply.github.com>
2024-11-21 18:54:14 +0900

    Add files via upload

---
+++ ready_screen_after.h
@@ -0,0 +1,11 @@
+#ifndef READY_SCREEN_AFTER_H
+#define READY_SCREEN_AFTER_H
+#include "player.h"
+
+void draw_ready_after(player* p, int day);
+void printInformation_after(player* p);
+void printInformation2_after(player* p);
+void draw_ready_text_after(player* p);
+void test_print(int x, int y);
+
+#endif
