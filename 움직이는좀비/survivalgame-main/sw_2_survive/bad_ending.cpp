74e92752642e2d103c0b11cc6fb88cc9797e2d4c
asdf6864 <134824544+asdf6864@users.noreply.github.com>
2024-11-21 16:18:14 +0900

    Add files via upload
    수정 
    day0.cpp
    day1.cpp
    day2.cpp
    zombie_move.h
    zombie_move.cpp
    expire_tool.cpp

---
+++ 움직이는좀비/survivalgame-main/sw_2_survive/bad_ending.cpp
@@ -0,0 +1,10 @@
+#include<windows.h>
+#include<iostream>
+
+void bad_ending() {
+	system("cls");
+	std::cout << "bad end";
+	while (1) {
+
+	}
+}
---
+++ 움직이는좀비/survivalgame-main/sw_2_survive/item_image.h
@@ -0,0 +1,4 @@
+#ifndef ITEM_IMAGE_H
+#define ITEM_IMAGE_H
+extern char itemList[5][10][16];
+#endif
