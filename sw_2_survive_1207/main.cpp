483b2bf438b8a7ed7918a6db5bfd5bf3f70c1408
Junyoung1225 <129856252+Junyoung1225@users.noreply.github.com>
2024-12-07 12:50:08 +0900

    12월 7일 코드통합

---
+++ sw_2_survive_1207/main.cpp
@@ -0,0 +1,89 @@
+#include"expire_tool.h"
+#include"day_0.h"
+#include"game_start.h"
+#include <Windows.h>
+#include "ready_screen.h"
+#include "ready_screen_after.h"
+#include"player.h"
+#include "day_1.h"
+#include "day_2.h"
+#include "day_3.h"
+#include "day_4.h"
+#include "day_7.h"
+#include "Day_5.h"
+#include "backpack.h"
+
+void deleteCursorBlink();
+
+int main() {
+	keybd_event(VK_MENU, 0, 0, 0);           // Alt 키 누름
+	keybd_event(VK_RETURN, 0, 0, 0);         // Enter 키 누름
+	keybd_event(VK_MENU, 0, KEYEVENTF_KEYUP, 0); // Alt 키 떼기
+	keybd_event(VK_RETURN, 0, KEYEVENTF_KEYUP, 0);
+
+
+
+
+
+	deleteCursorBlink();
+	
+	
+	
+	game_start_screen();
+	player* user = new player();
+	BackP* user_back = new BackP();
+	system("cls");
+	print_intro(0);
+	system("cls");
+	/*
+	start_day0(user, user_back);
+
+	system("cls");
+	draw_ready(user,1);
+	system("cls");
+
+	start_day1(user, user_back);
+
+	system("cls");
+	draw_ready_after(user, 1, user_back);
+	system("cls");
+	draw_ready(user,2);
+	system("cls");
+
+	start_day2(user, user_back);
+
+	system("cls");
+	draw_ready_after(user, 2, user_back);
+	system("cls");
+	draw_ready(user,3);
+	system("cls");
+
+	start_day3(user,user_back);
+
+	system("cls");
+	draw_ready_after(user, 3, user_back);
+	system("cls");
+	draw_ready(user, 4);
+	system("cls");
+
+	start_day4(user, user_back);
+	*/
+	//start_day5(user, user_back);
+	system("cls");
+	//draw_ready_after(user, 4, user_back);
+
+	start_day7(user, user_back);
+}
+
+
+
+
+void deleteCursorBlink() {
+	HANDLE hc = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO curCursorInfo;
+
+	GetConsoleCursorInfo(hc, &curCursorInfo);
+
+	curCursorInfo.bVisible = 0;
+	SetConsoleCursorInfo(hc, &curCursorInfo);
+}
---
+++ sw_2_survive_1207/npc_image.cpp
@@ -0,0 +1,96 @@
+﻿#include <iostream>
+#include"expire_tool.h"
+#ifdef _WIN32
+#include <windows.h>
+#endif
+
+
+void printplayer() {
+
+    UINT oldCP = GetConsoleOutputCP();
+
+#ifdef _WIN32
+    // Windows에서 UTF-8 인코딩 설정
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+    int x = 72;
+    int y = 10;
+    setCursorPosition(x, y);
+    // 유니코드 출력
+    std::cout << u8"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⠠⠤⠄⢀⠀⠀⠀⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 1);
+    std::cout << u8"⠀⠀⠀⠀⠀⠀⡀⠤⠒⠉⠉⠉⠁⠀⠀⠀⠀⠠⢀⡀⠀⠀⠀";
+    setCursorPosition(x, y + 2);
+    std::cout << u8"⠀⠀⠀⠀⠈⣩⠏⠀⠀⠀⠄⠀⠀⠀⠀⠀⠀⠀⠀⠈⣆⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 3);
+    std::cout << u8"⠀⠀⠀⠀⢘⡮⠀⢀⣾⣽⡄⠀⢰⡀⢀⠀⠀⠀⠀⠀⠹⡂⠀⠀⠀⠀";
+    setCursorPosition(x, y + 4);
+    std::cout << u8"⠀⠀⠀⠀⠸⣧⢰⣼⣻⣹⢣⢰⡿⡿⡌⣧⣆⢣⠀⠀⠀⡇⠀⠀⠀⠀";
+    setCursorPosition(x, y + 5);
+    std::cout << u8"⠀⠀⠀⠀⠀⠈⢿⣃⣀⣈⡁⠙⠋⢉⣉⣉⡓⣹⣦⡄⣄⠁⠀⠀⠀⠀";
+    setCursorPosition(x, y + 6);
+    std::cout << u8"⠀⠀⠀⠀⠀⠀⠸⡄⠧⠼⠁⡆⠈⠣⡠⠃⠁⠋⠀⢱⠋⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 7);
+    std::cout << u8"⠀⠀⠀⠀⠀⠀⠀⡇⠀⠀⠈⠀⠀⠀⠀⠀⠀⣀⠴⠃⠀⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 8);
+    std::cout << u8"⠀⠀⠀⠀⠀⠀⠀⠈⢦⡀⠀⠠⠄⠀⠀⡠⢊⠇⠀⠀⠀⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 9);
+    std::cout << u8"⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠲⡤⠤⠒⠉⠀⢸⡀⠀⠀⠀⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 10);
+    std::cout << u8"⠀⠀⠀⠀⠀⠀⠀⢀⣠⠴⡞⠁⠀⠀⠀⠀⠀⣹⠖⠤⣀⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 11);
+    std::cout << u8"⠀⠀⠀⣀⡤⣔⡎⠍⠀⠀⠘⢂⢀⣀⡀⠤⠒⠁⠀⠤⣄⣌⠑⠒⠤⡀";
+
+    SetConsoleOutputCP(oldCP);
+
+
+}
+
+void printnpc1() {
+    UINT oldCP = GetConsoleOutputCP();
+
+#ifdef _WIN32
+    // Windows에서 UTF-8 인코딩 설정
+    SetConsoleOutputCP(CP_UTF8);
+#endif
+    int x = 72;
+    int y = 10;
+    setCursorPosition(x, y);
+    // 유니코드 출력
+    std::cout << u8"⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣀⠬⢕⣖⢄⠀⠀⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 1);
+    std::cout << u8"⠀⠀⠀⠀⠀⠀⣠⡾⠛⠉⠀⠀⠀⠀⠁⠃⠠⢄⠀⠀⠀⠀";
+    setCursorPosition(x, y + 2);
+    std::cout << u8"⠀⠀⠀⠀⢠⠾⠁⠀⢀⣴⣋⣻⣷⡄⠀⠀⠀⠈⢳⠀⠀⠀⠀";
+    setCursorPosition(x, y + 3);
+    std::cout << u8"⠀⠀⠀⢠⣯⠆⠀⠀⢸⠁⠀⠀⠀⠁⠑⢦⡀⠀⠐⣧⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 4);
+    std::cout << u8"⠀⠀⠀⢸⣾⠀⠃⢸⣷⠲⠀⠀⠠⠶⠤⢤⢷⠀⠀⣯⠀⠀⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 5);
+    std::cout << u8"⠀⠀⠀⠀⠀⣸⣺⠤⠤⠤⡟⠀⠧⣤⣤⣀⡾⠁⢀⡇⠀⠀⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 6);
+    std::cout << u8"⠀⠀⠀⠀⢀⠗⠙⢆⠀⠀⠀⠀⠀⠀⠀⠀⣤⡖⠋⠀⠀⠀⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 7);
+    std::cout << u8"⠀⠀⠀⠀ ⠀⠀⠈⠳⣄⠈⠛⠀⣀⡤⢾⠏⠃⠀⠀⠀⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 8);
+    std::cout << u8"⠀⠀⠀⠀⠀⠀⠀⠀⠀⢈⡽⣍⣉⣡⠤⠞⢲⣄⡀⠀⠀⠀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 9);
+    std::cout << u8"⠀⠀⠀⠀⠀⠀⣀⡴⠛⢹⣴⢋⠳⡇⣀⡤⡞⠂⢹⠣⢄⡀⠀⠀⠀⠀";
+    setCursorPosition(x, y + 10);
+    std::cout << u8"⠀⠀⣀⣤⠶⢋⠏⠀⠀⡇⠛⠈⠆⠉⠁⢸⠁⠀⢸⡇⠀⠈⠓⢦⡀⠀";
+    setCursorPosition(x, y + 11);
+    std::cout << u8"⣠⠿⠋⠀⠀⠿⣲⠟⢠⠇⠀⢰⠀⠀⠀⣼⠠⣖⠚⠃⠀⠀⠀⠀⠈⢢";
+
+    SetConsoleOutputCP(oldCP);
+
+}
+
+
+void clear_npc_screen() {
+    int x = 72;
+    int y = 10;
+    for (int i = 0; i < 12; i++) {
+        setCursorPosition(x, y+i);
+        std::cout << "                                ";
+    }
+}
