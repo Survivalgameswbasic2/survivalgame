f485bb71f2cfba5520bad2b0302c1f0b9e6135ee
Junyoung1225 <129856252+Junyoung1225@users.noreply.github.com>
2024-12-17 21:20:16 +0900

    Delete sw_2_update_last directory

--- sw_2_update_last/day_1.h
+++
@@ -1,7 +0,0 @@
-#ifndef DAY_1_H
-#define DAY_1_H
-
-
-
-void start_day1(player* user, BackP* user_back);
-#endif
--- sw_2_update_last/map_image.h
+++
@@ -1,5 +0,0 @@
-#ifndef MAP_IMAGE_H
-#define MAP_IMAGE_H
-
-extern char mapList[7][6][20];
-#endif
